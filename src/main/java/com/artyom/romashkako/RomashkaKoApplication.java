c54557d40b9e754254572de59f50499df306cb3f
Artyom Hackimullin <xakimullin2@gmail.com>
2024-07-22 14:59:44 +0300

    Commit init

---
+++ src/main/java/com/artyom/romashkako/RomashkaKoApplication.java
@@ -0,0 +1,13 @@
+package com.artyom.romashkako;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomashkaKoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RomashkaKoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/artyom/romashkako/RomashkaKoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artyom.romashkako;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomashkaKoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
